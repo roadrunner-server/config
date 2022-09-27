6421c626891220ba4981a2a380c27e5ca7a4dc90
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-09-27 23:51:32 +0200

    fix: properly expand env variables
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugin.go
+++ plugin.go
@@ -67,12 +67,28 @@ func (p *Plugin) Init() error { //nolint:gocognit,gocyclo
 	// automatically inject ENV variables using ${ENV} pattern
 	for _, key := range p.viper.AllKeys() {
 		val := p.viper.Get(key)
-		str, ok := val.(string)
-		if !ok {
+		switch t := val.(type) {
+		case string:
+			// for string just expand it
+			p.viper.Set(key, os.ExpandEnv(t))
+		case []any:
+			// for slice -> check if it's slice of strings
+			strArr := make([]string, 0, len(t))
+			for i := 0; i < len(t); i++ {
+				if valStr, ok := t[i].(string); ok {
+					strArr = append(strArr, os.ExpandEnv(valStr))
+				} else {
+					p.viper.Set(key, os.ExpandEnv(valStr))
+				}
+			}
+
+			// we should set a whole array
+			if len(strArr) > 0 {
+				p.viper.Set(key, strArr)
+			}
+		default:
 			p.viper.Set(key, val)
-			continue
 		}
-		p.viper.Set(key, os.ExpandEnv(str))
 	}
 
 	// override config Flags
